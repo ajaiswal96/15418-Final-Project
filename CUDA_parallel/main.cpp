cbb045f07bb0d113dd1b2a3eb97da3c01cc670b9
Rohit Pillai <rrpillai@andrew.cmu.edu>
2017-05-10 23:00:37 -0400

    CUDA stuff is compiling

--- CUDA_parallel/main.cpp
+++ CUDA_parallel/main.cpp
@@ -1,4 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include 
+#include "parallel.h"
+
+int main(int argc, char ** argv) {
+    parallel();
+}
---
+++ CUDA_parallel/parallel.h
@@ -0,0 +1,6 @@
+#ifndef __PARALLEL_H__
+#define __PARALLEL_H__
+
+void parallel();
+
+#endif /*__PARALLEL_H__*/
