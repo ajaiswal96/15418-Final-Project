802ad6d6eaa0310641c678d635d66f9d5f580e93
Rohit Pillai <rrpillai@GHC44.GHC.ANDREW.CMU.EDU>
2017-05-11 00:17:31 -0400

    Parallel shit finally compiles on GHC

---
+++ CUDA_parallel/build_dir/CMakeFiles/PBF_15418.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/afs/andrew.cmu.edu/usr22/rrpillai/private/15418/15418-Final-Project/CUDA_parallel/main.cpp" "/afs/andrew.cmu.edu/usr22/rrpillai/private/15418/15418-Final-Project/CUDA_parallel/build_dir/CMakeFiles/PBF_15418.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/afs/andrew.cmu.edu/usr8/efang/public/glm"
+  "/usr/include/libdrm"
+  "/usr/local/depot/cuda-8.0/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CUDA_parallel/build_dir/CMakeFiles/PBF_15418.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/PBF_15418.dir/./PBF_15418_generated_parallel.cu.o"
+  "CMakeFiles/PBF_15418.dir/main.cpp.o"
+  "PBF_15418.pdb"
+  "PBF_15418"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/PBF_15418.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
